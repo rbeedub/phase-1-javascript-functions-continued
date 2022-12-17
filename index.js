ca590b95682497abc2ea766190183f1e14871296
Rebecca Wollard <rbwollard@gmail.com>
2022-12-16 20:49:35 -0700

    commit tree 847e5a7051ac89177eef781f5e7c79457ff4e18b

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+
+function saturdayFun(placeholder = "roller-skate") {
+    return(`This Saturday, I want to ${placeholder}!`)
+};
+
+saturdayFun("bathe my dog");
+
+
+function mondayWork(holdPlace = "go to the office") {
+    return(`This Monday, I will ${holdPlace}.`)
+};
+
+mondayWork("work from home");
+
+function wrapAdjective (flair = "*") {
+    return function(adj = "special") {
+        return (`You are ${flair}${adj}${flair}!`)
+    };
+}
--- test/indexTest.js
+++ test/indexTest.js
@@ -47,3 +47,4 @@ describe("index.js", () => {
     });
   })
 })
+
